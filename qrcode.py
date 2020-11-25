4c226edbdb7e46a829662399c8764ef939c9ffd4
Monish <66905892+monish-mnjds@users.noreply.github.com>
2020-11-25 12:46:03 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,1 +1,9 @@
+from PIL import Image
+import qrcode
 
+url = 'https://github.com/monish-mnjds'
+file_format = 'High_courts_of_India.png'
+img = qrcode.make(url)
+img.save(file_format)
+im = Image.open(file_format)
+im.show()
